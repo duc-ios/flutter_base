5d1876dfd521abbeda4414d24f3434b374ae55b7
Duc iOS <duc.nh2012@gmail.com>
2022-10-13 20:24:13 +0700

    refactor: custom fonts

--- lib/src/common/utils/app_theme.dart
+++ lib/src/common/utils/app_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../generated/colors.gen.dart';
+import '../../../generated/fonts.gen.dart';
 
 class AppTheme {
   static TextTheme get textTheme => const TextTheme();
@@ -9,4 +10,30 @@ class AppTheme {
     primarySwatch: ColorName.materialPrimary,
     textTheme: textTheme,
   );
+}
+
+extension TextThemeX on TextTheme {
+  TextStyle get regular => const TextStyle(
+        color: ColorName.black,
+        fontSize: 16,
+        fontFamily: FontFamily.roboto,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0,
+      );
+
+  TextStyle get medium => const TextStyle(
+        color: ColorName.black,
+        fontSize: 16,
+        fontFamily: FontFamily.roboto,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0,
+      );
+
+  TextStyle get bold => const TextStyle(
+        color: ColorName.black,
+        fontSize: 16,
+        fontFamily: FontFamily.roboto,
+        fontWeight: FontWeight.w700,
+        letterSpacing: 0,
+      );
 }
