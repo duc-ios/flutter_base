5ed028db6d89240e74567bae4cf02ee549daac9b
Duc iOS <duc.nh2012@gmail.com>
2022-10-14 17:47:42 +0700

    refactor: logger

--- lib/src/common/utils/logger.dart
+++ lib/src/common/utils/logger.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_loggy/flutter_loggy.dart';
 import 'package:loggy/loggy.dart';
+import 'package:stack_trace/stack_trace.dart';
 
 final logger = _Logger();
 
@@ -21,17 +22,21 @@ class _Logger {
   String _colorFor(String level) => '\x1B[${_colors[level]}m';
 
   // White text
-  d(dynamic msg) => logDebug('${_colorFor('d')}${msg.toString()}\x1B[0m');
+  d(dynamic msg) =>
+      logDebug('${_traceInfo()}\n${_colorFor('d')}${msg.toString()}\x1B[0m');
 
   // Blue text
-  i(dynamic msg) => logInfo('${_colorFor('i')}${msg.toString()}\x1B[0m');
-
-  // Green text
-  s(dynamic msg) => logInfo('${_colorFor('s')}${msg.toString()}\x1B[0m');
+  i(dynamic msg) =>
+      logInfo('${_traceInfo()}\n${_colorFor('i')}${msg.toString()}\x1B[0m');
 
   // Yellow text
-  w(dynamic msg) => logWarning('${_colorFor('w')}${msg.toString()}\x1B[0m');
+  w(dynamic msg) =>
+      logWarning('${_traceInfo()}\n${_colorFor('w')}${msg.toString()}\x1B[0m');
 
   // Red text
-  e(dynamic msg) => logError('${_colorFor('e')}${msg.toString()}\x1B[0m');
+  e(dynamic msg) =>
+      logError('${_traceInfo()}\n${_colorFor('e')}${msg.toString()}\x1B[0m');
+
+  String _traceInfo() =>
+      Trace.from(StackTrace.current).terse.frames[2].toString();
 }
