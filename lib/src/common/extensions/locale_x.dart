d9249f006fe5a5640ea06ba9aaa3473fb6341d1c
JamesVu33 <vhtrungdev@gmail.com>
2023-05-26 12:48:32 +0700

    refactor(lang): cover all China locale string types (#9)
    * refactor(lang): cover all China locale string types
    
    * refactor(lang): update feedback
    
    ---------
    
    Co-authored-by: Vu Trung <james.vu@legato.co>

--- lib/src/common/extensions/locale_x.dart
+++ lib/src/common/extensions/locale_x.dart
@@ -34,6 +34,22 @@ extension LocaleX on Locale {
   String get fullLanguageCode =>
       [languageCode, scriptCode].where((element) => element != null).join('_');
 
+  bool isEqualTo(Locale compare) {
+    if (this == compare) {
+      return true;
+    }
+    if (scriptCode?.isNotEmpty == true && scriptCode == compare.scriptCode) {
+      return true;
+    }
+    if ((scriptCode?.isEmpty == true || compare.scriptCode?.isEmpty == true)
+        && countryCode?.isNotEmpty == true
+        && countryCode == compare.countryCode
+    ) {
+      return true;
+    }
+    return false;
+  }
+
   static List<Locale> get supportedLocales =>
       List<Locale>.from(S.delegate.supportedLocales)
         ..sort((l, r) {
@@ -48,12 +64,32 @@ extension StringLocale on String {
   Locale get toLocale {
     final components = split('_');
     if (components.length >= 2 && components[0] == 'zh') {
-      return Locale.fromSubtags(
-          languageCode: components[0], scriptCode: components[1]);
+      return _getPlatformLanguage(components);
     } else if (components.isNotEmpty) {
       return Locale(components[0]);
     } else {
       return LocaleX.fallbackLocale;
     }
   }
+
+  Locale _getPlatformLanguage(List<String> components) {
+    if (ZHStringSupport.zhTraditionalString.contains(this)) {
+      return const Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hant');
+    }
+    if (ZHStringSupport.zhSimplifiedString.contains(this)) {
+      return const Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hans');
+    }
+    return Locale.fromSubtags(
+        languageCode: components[0], scriptCode: components[1]);
+  }
+}
+
+class ZHStringSupport {
+  static const zhTraditionalString = [
+    'zh_Hant', 'zh_Hant_TW', 'zh_Hant_HK', 'zh_TW', 'zh_HK', 'zh_CHT'
+  ];
+
+  static const zhSimplifiedString = [
+    'zh_Hans', 'zh_Hans_CN', 'zh_CN', 'zh_CHS', 'zh_SG'
+  ];
 }
